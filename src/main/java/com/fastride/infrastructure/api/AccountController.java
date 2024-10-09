8ed3d43f145f3612a2d04db02971a28f25e9972a
João Victor <joaovictor.cogomes@gmail.com>
2024-10-09 19:14:52 -0300

    Creating AccountController with endpoints for create and retrieve account, along with dtos for input and output

---
+++ src/main/java/com/fastride/infrastructure/api/AccountController.java
@@ -0,0 +1,51 @@
+package com.fastride.infrastructure.api;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fastride.domain.account.model.Account;
+import com.fastride.domain.account.usecase.GetAccountUseCase;
+import com.fastride.domain.account.usecase.SignUpUseCase;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+	private SignUpUseCase signUpUseCase;
+	private GetAccountUseCase getAccountUseCase;
+	private AccountConverter accountConverter;
+
+	public AccountController(SignUpUseCase signUpUseCase, GetAccountUseCase getAccountUseCase,
+			AccountConverter accountConverter) {
+		this.signUpUseCase = signUpUseCase;
+		this.getAccountUseCase = getAccountUseCase;
+		this.accountConverter = accountConverter;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public AccountOutputDto signUp(@RequestBody @Valid AccountInputDto accountInput) {
+		Account account = this.accountConverter.toEntity(accountInput);
+		account = this.signUpUseCase.execute(account);
+		return this.accountConverter.toOutputDto(account);
+	}
+
+	@GetMapping("/{accountId}")
+	@ResponseStatus(HttpStatus.OK)
+	public AccountOutputDto getAccountById(@PathVariable String accountId) {
+		// TODO: create validation for UUID (encapsulate UUID and id logic in a Value
+		// Object called AccountId)
+		return this.accountConverter.toOutputDto(this.getAccountUseCase.execute(UUID.fromString(accountId)));
+	}
+
+}
---
+++ src/main/java/com/fastride/infrastructure/api/AccountInputDto.java
@@ -0,0 +1,70 @@
+package com.fastride.infrastructure.api;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class AccountInputDto {
+
+	@NotBlank
+	private String name;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	@CPF
+	private String cpf;
+	private String carPlate;
+	private boolean driver;
+	private boolean passenger;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCarPlate() {
+		return carPlate;
+	}
+
+	public void setCarPlate(String carPlate) {
+		this.carPlate = carPlate;
+	}
+
+	public boolean isDriver() {
+		return driver;
+	}
+
+	public void setDriver(boolean driver) {
+		this.driver = driver;
+	}
+
+	public boolean isPassenger() {
+		return passenger;
+	}
+
+	public void setPassenger(boolean passenger) {
+		this.passenger = passenger;
+	}
+
+}
