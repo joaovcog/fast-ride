9000e866f427e02b049cf710743c4b87aaeb7c70
João Victor <joaovictor.cogomes@gmail.com>
2025-04-28 21:15:34 -0300

    Removing needless classes AccountValidator, CarPlateValidator, ExistingAccountValidator

--- src/main/java/com/fastride/domain/account/usecase/SignUpUseCase.java
+++ src/main/java/com/fastride/domain/account/usecase/SignUpUseCase.java
@@ -4,24 +4,29 @@ import org.springframework.stereotype.Component;
 
 import com.fastride.domain.account.model.Account;
 import com.fastride.domain.account.model.AccountRepository;
-import com.fastride.domain.account.validation.AccountValidator;
+import com.fastride.domain.account.model.Email;
 import com.fastride.domain.shared.EntityId;
+import com.fastride.domain.shared.ValidationException;
 
 @Component
 public class SignUpUseCase {
 
 	private AccountRepository accountRepository;
-	private AccountValidator accountValidator;
 
-	public SignUpUseCase(AccountRepository accountRepository, AccountValidator accountValidator) {
+	public SignUpUseCase(AccountRepository accountRepository) {
 		this.accountRepository = accountRepository;
-		this.accountValidator = accountValidator;
 	}
 
 	public Account execute(Account account) {
-		this.accountValidator.validate(account);
+		this.validateExistingAccount(account.getEmail());
 		account = new Account(new EntityId(), account);
 		return this.accountRepository.create(account);
+	}
+
+	private void validateExistingAccount(Email email) {
+		if (accountRepository.findByEmail(email.getContent()).isPresent())
+			throw new ValidationException(String.format("An account with the e-mail %s already exists! "
+					+ "Please, type another e-mail for creating a new account.", email.getContent()));
 	}
 
 }
--- src/main/java/com/fastride/domain/account/validation/Validator.java
+++
@@ -1,7 +0,0 @@
-package com.fastride.domain.account.validation;
-
-public interface Validator {
-
-	void validate(String value);
-
-}
